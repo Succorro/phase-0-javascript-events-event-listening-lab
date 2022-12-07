bd0ecfb09f54baff4855f2be156ad3b06e6ad9b0
Steven Gutierrez <stevogbmv@gmail.com>
2022-12-07 13:56:04 -0800

    commit tree d347d4e44ab8fc49d6dd91171e3fc056f1d29150

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-function addingEventListener() {
+// function addingEventListener() {
+//     const input = document.getElementById('button');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+// }
+function addingEventListener(){
+  return document.getElementById('button').addEventListener('click', () => {
+    alert ('I was clicked!')
+  })
 }
+addingEventListener();
